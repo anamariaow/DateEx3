86c4e4c6930cda0950e7163ad59e65c7532b0ad5
anamariaow <new_annemarie@yahoo.com>
2023-12-22 03:32:32 +0100

    Aggiunto secondo modo

--- src/Main.java
+++ src/Main.java
@@ -11,15 +11,21 @@ public class Main {
         String anno = data.format(DateTimeFormatter.ofPattern("yyyy"));
         //ottieni il mese
         String getMonth = String.valueOf(data.getMonth());
+        String mese = data.format(DateTimeFormatter.ofPattern("MMMM"));
         //ottieni il giorno
         String getDay = String.valueOf(data.getDayOfMonth());
+        String giorno = data.format(DateTimeFormatter.ofPattern("dd"));
         //ottieni il giorno della settimana
         String getWeekDay = String.valueOf(data.getDayOfWeek());
+        String giornoSettimana = data.format(DateTimeFormatter.ofPattern("EEEE"));
         //Stampa i risultati sulla console
         System.out.println("Anno: " + getYear);
         System.out.println("Anno yyyy: " + anno);
         System.out.println("Mese: " + getMonth);
+        System.out.println("Mese MMMM: " + mese);
         System.out.println("Giorno: " + getDay);
+        System.out.println("Giorno dd: " + giorno);
         System.out.println("Giorno della settimana: " + getWeekDay);
+        System.out.println("Giorno della settimana EEEE: " + giornoSettimana);
     }
 }
